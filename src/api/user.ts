7b09208abb07bc254067551a7abc0322d0c60cb3
EsterLev <st3281357@gmail.com>
2022-08-08 09:52:49 +0300

    api

--- src/api/user.ts
+++ src/api/user.ts
@@ -8,8 +8,48 @@ export const get = () => {
 
     }
     catch (error) {
-        console.log('error in get user', error);
+        console.log('error in get users', error);
     }
 
+}
+
+export const getById = (id: string) => {
+    try {
+        return axios.get('http://localhost:3333/user/'+id);
+
+    }
+    catch (error) {
+        console.log('error in get user', error);
+    }
+}
+
+export const update = (id: string, user: User) => {
+    try {
+        return axios.put('http://localhost:3333/user/'+id, user);
+
+    }
+    catch (error) {
+        console.log('error in update user', error);
+    }
+}
+
+export const add = (user: User) => {
+    try {
+        return axios.post('http://localhost:3333/user',user);
+
+    }
+    catch (error) {
+        console.log('error in add user', error);
+    }
+}
+
+export const deleteById = (id: string) => {
+    try {
+        return axios.delete('http://localhost:3333/user/'+id);
+
+    }
+    catch (error) {
+        console.log('error in delete user', error);
+    }
 }
 // export get;
