9c9613e9862ab14e87dcd06ce9094adbd9f2f572
Rachely-Braverman <rachelb55677@gmail.com>
2022-08-11 19:04:24 +0300

    systems

--- src/components/system.tsx
+++ src/components/system.tsx
@@ -5,15 +5,12 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { getSystems } from '../api/system';
-
 import { System } from '../models/system.model';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
-
-
 export default function Systems() {
-    
+
     const navigate = useNavigate();
 
     const [systems, setSystems] = useState([]);
@@ -35,27 +32,35 @@ export default function Systems() {
     }, [])
 
     return (
-        <div>
-        <ul>
-          {systems.map((s: System) => (
-            <Card sx={{ minWidth: 27 }}>
-              <CardContent>
-                <li key={String(s._id)}>
-                  {" "}
-                  <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-                    <Button variant="text"><Link to={`/system/${s.urlName}`}> {s.topic + ' ' + s.objectName + ' ' + s.description}</Link></Button>{" "}<br />
-                    {/* <Link to={`/addUser`}> to add user</Link>{" "}<br /> */}
-                    {/* <Link to={`/system/${s.urlName}`}> to show system</Link>{" "}<br /> */}
-                    {/* <Button variant="contained">Contained</Button>
-                    <Button variant="outlined">Outlined</Button> */}
-                  </Typography>
-                </li>
-              </CardContent>
-            </Card>
-          ))}
-        </ul>
-        <Link to={`/add`}> to add system</Link>{" "}<br />
-    </div >
+        <>
+            {systems?.map((s: System) => (
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <div key={String(s._id)}>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                {s.topic}
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                phone:{s.phone}
+                                <br />
+                                email:{s.email}
+                            </Typography>
+                            <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
+                                <Button variant="text"><Link to={`/${s._id}`}> {s.topic + '  ' + s.objectName + '  ' + s.description}</Link></Button>{" "}<br />
+                            </Typography>
+                            <CardActions>
+                                <Button size="small" onClick=
+                                    {
+                                        () => navigate(`/system/getSystemByUrlName/:urlName`,
+                                            { state: { idSystem: s._id } })
+                                    }
+                                >show system</Button>
+                            </CardActions>
+                        </div>
+                    </CardContent>
+                </Card>
+            ))}
+        </>
     );
 
 }
