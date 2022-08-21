af673e9af6998ca21a9d63d91ff2d553b8c01cf1
Rachely-Braverman <rachelb55677@gmail.com>
2022-08-21 16:18:59 +0300

    perfect deletion

--- src/components/showSystem.tsx
+++ src/components/showSystem.tsx
@@ -45,82 +45,39 @@ const ShowSystem = () => {
 
 
     const Delete = async () => {
-
-        let result = await deleteSystem(String(system._id));
-        console.log(result);
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this system file!",
+            icon: "warning",
+            dangerMode: true,
+        })
+            .then(async function name(willDelete: any) {
+                debugger;
+                console.log('name: ' + willDelete);
+                if (willDelete) {
+                    try {
+                        let result = await deleteSystem(String(system._id));
+                        console.log(result);
+                        swal("Poof! Your system has been deleted!", {
+                            icon: "success",
+                        });
+                    } catch (error) {
+                        console.error(error);
+                    }
+    
+                } else {
+                    swal("Your system is safe!");
+                }
+            });
         navigate('/systems');
+
+        // let result = await deleteSystem(String(system._id));
+        // console.log(result);
+        // navigate('/systems');
     };
 
-    //     return system ?
-    //         <Card>
-    //             <CardContent>
-    //                 <form className='auth-inner'
-    //                 //   component="form"
-    //                 //   sx={{
-    //                 //     '& > :not(style)': { m: 1, width: '25ch' },
-    //                 //   }}
-    //                 //   noValidate
-    //                 //   autoComplete="off"
-    //                 >
-    //                     {/* <h3>system {system._id}</h3> */}
-    //                     <Typography variant="h3">The system</Typography>
-
-    //                     {/* <TextField id="outlined-basic" label="topic" variant="outlined"  className="mb-3"/>
-    //   <TextField id="outlined-basic" label="objectName" variant="outlined" className="mb-3" /> */}
-    //                     {/* <TextField id="filled-basic" label="Filled" variant="filled" />
-    //   <TextField id="standard-basic" label="Standard" variant="standard" /> */}
-    //                     <div className="mb-3">
-    //                         {/* <label>topic:  {system.topic} </label> */}
-    //                         <Typography variant="h5">topic:  {system.topic}</Typography>
-
-    //                     </div>
-    //                     <div className="mb-3">
-    //                         <Typography variant="h5">objectName:   {system.objectName}</Typography>
-
-    //                     </div>
-    //                     <div className="mb-3">
-    //                         <Typography variant="h5">description:   {system.description}</Typography>
-    //                     </div>
-    //                     <div className="mb-3">
-    //                         <Typography variant="h5">urlName:   {system.urlName}</Typography>
-    //                     </div>
-    //                     <div className="mb-3">
-    //                         <Typography variant="h5">email address:   {system.email}</Typography>
-    //                     </div>
-    //                     <div className="mb-3">
-    //                         <Typography variant="h5">phone number:   {system.phone}</Typography>
-    //                     </div>
-    //                     <div className="d-grid">
-    //                         <Button variant="text" onClick={Delete}>to delete</Button>{" "}<br />
-    //                     </div>
-    //                 </form>
-    //             </CardContent>
-    //         </Card> : <h1>not system found</h1>;
-
-    swal({
-        title: "Are you sure?",
-        text: "Once deleted, you will not be able to recover this system file!",
-        icon: "warning",
-        dangerMode: true,
-    })
-        .then(async function name(willDelete: any) {
-
-            if (willDelete) {
-                try {
-                    let result = await deleteSystem(String(system._id));
-                    console.log(result);
-                    swal("Poof! Your system has been deleted!", {
-                        icon: "success",
-                    });
-                } catch (error) {
-                    console.error(error);
-                }
-
-            } else {
-                swal("Your system is safe!");
-            }
-        });
-    navigate('/systems');
+   
+    
 
 
     const topic: any = useRef();
@@ -134,7 +91,7 @@ const ShowSystem = () => {
     const Edit = async () => {
 
         const newSystem: System = {
-            "topic": topic.value,
+            "topic": topic.current?.value,
             "objectName": objectName.value,
             "ownerId": ownerId.value,
             "description": description.value,
@@ -142,7 +99,7 @@ const ShowSystem = () => {
             "phone": phone.value,
             "urlName": URLName.value
         }
-
+        console.log("newSystem: " + newSystem);
         swal({
             title: "Are you sure?",
             text: "Once edited, you will not be able to recover this system file!",
@@ -153,6 +110,7 @@ const ShowSystem = () => {
 
                 if (willDelete) {
                     try {
+                        debugger;
                         let result = await updateSystem(String(system._id), newSystem);
                         console.log(result);
                         swal("Poof! Your system has been edited!", {
