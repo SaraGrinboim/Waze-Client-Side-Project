e34df9a31bce92f7d2a0c1d3a332d387aed0e287
EstiLev <st3281357@gmail.com>
2022-08-24 12:16:01 +0300

    autocomplete

--- src/components/autocomplete.tsx
+++ src/components/autocomplete.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import google from 'google';
-<script async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBL9SengOBv22kYKJDCPRUSvgt_orH7q0M&libraries=places&callback=initMap">
-</script>
+
+
 const AutoComplete = () => {
     const center = { lat: 50.064192, lng: -130.605469 };
     // Create a bounding box with sides ~10km away from the center point
@@ -12,22 +11,49 @@ const AutoComplete = () => {
         east: center.lng + 0.1,
         west: center.lng - 0.1,
     };
-    const input = document.getElementById("pac-input") as HTMLInputElement;
-    const options = {
-        bounds: defaultBounds,
-        componentRestrictions: { country: "us" },
-        fields: ["address_components", "geometry", "icon", "name"],
-        strictBounds: false,
-        types: ["establishment"],
-    };
+    useEffect(() => {
+        const input = document.getElementById("searchTextField") as HTMLInputElement;
+        const options = {
+            bounds: defaultBounds,
+            componentRestrictions: { country: "us" },
+            fields: ["address_components", "geometry", "icon", "name"],
+            strictBounds: false,
+            types: ["establishment"],
+        };
 
-    const autocomplete = new google.maps.places.Autocomplete(input, options);
-    autocomplete.setFields(["place_id", "geometry", "name"]);
+        const autocomplete = new window.google.maps.places.Autocomplete(input, options);
+        autocomplete.setFields(["place_id", "geometry", "name"]);
+    },[]);
+
+
+
+    // window.google.maps.event.addDomListener(window, 'load', initialize);
+
 
     return (
-        <div>
+        // <div>
+        //     <input id="searchTextField" type="text" size={50} placeholder="Anything you want!" />
+        // </div>
 
-            <input id="searchTextField" type="text" size={50} placeholder="Anything you want!" />
+
+<div>
+        <header>
+            <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/handlebars/4.7.7/handlebars.min.js"></script>
+            <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></link>
+            <h1 className="search-title">
+                <img src="https://fonts.gstatic.com/s/i/googlematerialicons/place/v15/24px.svg" />
+                Find a location near you
+            </h1>
+            <div className="search-input">
+                <input id="searchTextField" placeholder="Enter your address or zip code" />
+                <div id="search-overlay-search" className="search-input-overlay search">
+                    <button id="location-search-button">
+                        <img className="icon" src="https://fonts.gstatic.com/s/i/googlematerialicons/search/v11/24px.svg" alt="Search" />
+                    </button>
+                </div>
+            </div>
+        </header>
         </div>
     )
 
