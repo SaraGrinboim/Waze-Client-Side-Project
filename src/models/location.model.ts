642111d4bbc14a5bcf43cd2f0a2932752d0d0440
Rachely-Braverman <rachelb55677@gmail.com>
2022-09-04 11:06:04 +0300

    location model

---
+++ src/models/location.model.ts
@@ -0,0 +1,11 @@
+export type Location = {
+
+    manager_id: String;
+    system_id: String;
+    location: {lat: Number, lng: Number};
+    description: String;
+    name: String;
+    notes: String;
+    communication: String;
+
+}
