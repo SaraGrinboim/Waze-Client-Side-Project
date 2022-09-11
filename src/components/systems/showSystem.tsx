0be033bc316b64dd8da978647bfefc7d9ee03654
EstiLev <st3281357@gmail.com>
2022-09-11 18:08:21 +0300

    abcd

--- src/components/systems/showSystem.tsx
+++ src/components/systems/showSystem.tsx
@@ -1,49 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { observer } from 'mobx-react';
 import systemStore from '../../api/system';
-import { Button, Card, CardContent, Typography, TextField } from '@mui/material';
+import { Button, Card, CardContent, Typography, CardActionArea } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
 import swal from 'sweetalert';
 import { System } from '../../models/system.model';
 import '../../styles/system.css';
 import userStore from '../../api/user';
+import CardMedia from '@mui/material/CardMedia';
 
 const ShowSystem = () => {
 
-    let s: System = {
-        topic: "",
-        objectName: "",
-        ownerId: "",
-        description: "",
-        email: "",
-        phone: "",
-        urlName: "",
-        logoUrl: "",
-    };
     const navigate = useNavigate();
     const { urlName, id } = useParams();
     const [edit, setEdit] = useState(false);
-
-    useEffect(() => {
-        async function getSystem() {
-            if (id) {
-                await systemStore.getSystemById(String(id))
-            }
-            else {
-                await systemStore.getSystemsByUrlName(String(urlName))
-            }
-
-            if (!systemStore.system) {
-                alert('no system found');
-                navigate('/systems');
-            }
-        }
-        getSystem();
-    }, []);
-
-
     const Delete = async () => {
         swal({
             title: "Are you sure?",
@@ -69,54 +41,25 @@ const ShowSystem = () => {
                 }
             });
         navigate('/systems');
-
-
     };
+    
+    useEffect(() => {
+        async function getSystem() {
+            if (id) {
+                await systemStore.getSystemById(String(id))
+            }
+            else {
+                await systemStore.getSystemsByUrlName(String(urlName))
+            }
 
-    const topic: any = useRef();
-    const objectName: any = useRef();
-    const description: any = useRef();
-    const email: any = useRef();
-    const phone: any = useRef();
-    const URLName: any = useRef();
-    const LogoUrl: any = useRef();
-
-    const Edit = async () => {
-        const newSystem: System = {
-            "topic": topic.current?.value,
-            "objectName": objectName.current?.value,
-            "ownerId": systemStore.system.ownerId,
-            "description": description.current?.value,
-            "email": email.current?.value,
-            "phone": phone.current?.value,
-            "urlName": URLName.current?.value,
-            "logoUrl": LogoUrl.current?.value
+            if (!systemStore.system) {
+                alert('no system found');
+                navigate('/systems');
+            }
         }
-        console.log("newSystem: " + newSystem);
+        getSystem();
+    }, []);
 
-        swal({
-            title: "Are you sure?",
-            text: "Once edited, you will not be able to recover this system file!",
-            icon: "warning",
-            dangerMode: true,
-        })
-            .then(async function name(willDelete: any) {
-                if (willDelete) {
-                    try {
-                        let result = await systemStore.updateSystem(String(systemStore.system._id), newSystem);
-                        swal("Poof! Your system has been edited!", {
-                            icon: "success",
-                        });
-                    } catch (error) {
-                        console.error(error);
-                    }
-                } else {
-                    swal("Your system is safe!");
-                }
-            });
-        navigate('/systems');
-
-    };
     return (
         <>
             {
@@ -154,36 +97,6 @@ const ShowSystem = () => {
                         </form>
                     </CardContent>
                 </Card>
-            }
-            {
-                edit &&
-                <form className='edit' onSubmit={Edit}>
-                    <h3>change this system</h3>
-                    <div className="mb-3">
-                        <TextField required type="string" id="outlined-basic" label="enter topic" variant="outlined" inputRef={topic}></TextField>
-                    </div>
-                    <div className="mb-3">
-                        <TextField required type="string" id="outlined-basic" label="enter objectName" variant="outlined" inputRef={objectName}></TextField>
-                    </div>
-                    <div className="mb-3">
-                        <TextField required type="string" id="outlined-basic" label="enter description" variant="outlined" inputRef={description}></TextField>
-                    </div>
-                    <div className="mb-3">
-                        <TextField required type="email" id="outlined-basic" label="enter email address" variant="outlined" inputRef={email}></TextField>
-                    </div>
-                    <div className="mb-3">
-                        <TextField required type="number" id="outlined-basic" label="enter phone number" variant="outlined" inputRef={phone}></TextField>
-                    </div>
-                    <div className="mb-3">
-                        <TextField required type="string" id="outlined-basic" label="enter urlName" variant="outlined" inputRef={URLName}></TextField>
-                    </div>
-                    <div className="mb-3">
-                        <TextField required type="string" id="outlined-basic" label="enter logo url" variant="outlined" inputRef={LogoUrl}></TextField>
-                    </div>
-                    <div className="d-grid">
-                        <Button variant="outlined" type="submit">Submit</Button>
-                    </div>
-                </form>
             }
         </>
     );
